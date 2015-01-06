c04a2f021ab24f7f96fd08e3f403ec2e068ca414
Johnny Xu <johnny.xu@sportsbet.com.au>
2015-01-06 17:36:08 +1100

    unified header styles between views and added firebase google auth test

--- app/js/dash-ctrl.js
+++ app/js/dash-ctrl.js
@@ -151,4 +151,38 @@ angular.module('playground')
 		}
 	};
 
+	// login
+	var ref = new Firebase("https://patternplayground.firebaseio.com");
+	ref.onAuth(function(authData){
+		if (authData == null) {
+			console.log("User logged out");
+			$scope.user = {
+				id: undefined,
+				name: undefined,
+				picture: undefined
+			};
+		} else {
+			console.log("User logged in: ", authData);
+			var userdata = authData[authData.provider];
+$scope.user = {
+					id: authData.uid,
+					name: userdata.displayName,
+					picture: userdata.cachedUserProfile.picture
+				};
+		}
+	});
+
+	$scope.login = function(){
+		ref.authWithOAuthPopup("google", function(error, authData) {
+			if (error) {
+				console.log("Login Failed!", error);
+			}
+		});
+	}
+
+	// logout
+	$scope.logout = function(){
+		ref.unauth();
+	}
+
 }]);
